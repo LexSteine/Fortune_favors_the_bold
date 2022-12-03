ca2872de5d4ca1ead9b4078ada9e95018e66e02c
LexSteine <112965317+LexSteine@users.noreply.github.com>
2022-12-03 10:56:57 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,8 @@
 def squirrel(N):
-    if N == 0:
-        factorial = N
-    else:
-        factorial = 1
-        while N > 0:
-            factorial = factorial * N
-            N -= 1
-        while factorial > 10:
-            factorial //= 10
+    factorial = 1
+    while N > 0:
+        factorial = factorial * N
+        N -= 1
+    while factorial > 10:
+        factorial //= 10
     return factorial
