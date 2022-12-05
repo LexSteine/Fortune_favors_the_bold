371e2b40a9ec8ba43c085991f14609c31643b042
LexSteine <112965317+LexSteine@users.noreply.github.com>
2022-12-05 22:05:02 +0300

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,16 @@
+def MadMax(N, Tele):
+    if N > 1:
+        i, j = 0, 1
+        while i < N:
+            while j < N:
+                if Tele[i] > Tele[j]:
+                    Tele[i],Tele[j] = Tele[j],Tele[i]
+                j += 1
+            i += 1
+            j = i + 1
+        c = 0
+        while c < N//2:
+            if Tele[N//2+c] < Tele[N-1-c]:
+                Tele[N//2+c],Tele[N-1-c] = Tele[N-1-c],Tele[N//2+c]
+            c += 1
+    return Tele
