768a5a9cc8440b72a1d81caa74cfd47f9a6d0ebd
LexSteine <112965317+LexSteine@users.noreply.github.com>
2022-12-07 20:16:00 +0300

    Add files via upload

---
+++ Task5.py
@@ -0,0 +1,29 @@
+def SynchronizingTables(N, ids, salary):
+    if N > 1:
+        seq = []
+        for i in ids:
+            seq.append(i)
+
+        def sort(N, spis):
+            i, j = 0, 0
+            while i < N:
+                while j < N:
+                    if spis[j] < spis[i]:
+                        spis[i], spis[j] = spis[j], spis[i]
+                    j += 1
+                i += 1
+                j = i + 1
+            return spis
+        salary = sort(N, salary)
+        seq = sort(N, seq)
+
+        def re(m1, m2, m3):
+            for i in m2:
+                for j in m1:
+                    if j == m3[m2.index(i)] and m1.index(j) != m2.index(i):
+                        m3[m2.index(i)], m3[m1.index(j)] = m3[m1.index(j)], m3[m2.index(i)]
+                        m2[m2.index(i)], m2[m1.index(j)] = m2[m1.index(j)], m2[m2.index(i)]
+            return m2
+
+        return re(ids, salary, seq)
+    return salary
