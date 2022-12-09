5132b7204b7b0061ef1d97cdd233135a877582db
LexSteine <112965317+LexSteine@users.noreply.github.com>
2022-12-09 23:34:23 +0300

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -1,20 +1,10 @@
 def PatternUnlock(N, hits):
     Points = {1: [1, 2], 2: [2, 2], 3: [3, 2], 4: [3, 1], 5: [2, 1], 6: [1, 1], 7: [3, 3], 8: [2, 3], 9: [1, 3]}
-
-    def checkOnDiagonal(current, next):
-        if 1 < (abs(Points.get(current)[0] - Points.get(next)[0]) +
-                abs(Points.get(current)[1] - Points.get(next)[1])):
-            return True
-        return False
-
-    def toFixed(number, digits=0):
-        return f"{number:.{digits}f}"
-
     for i in range(0, len(hits) - 1):
-        if checkOnDiagonal(hits[i], hits[i + 1]):
+        if checkOnDiagonal(Points, hits[i], hits[i + 1]):
             N += 0.414213562
     N = toFixed(N - 1, 5).split(".")
-    result= ""
+    result = ""
     for j in N[0]:
         if j != "0":
             result += j
@@ -22,3 +12,14 @@ def PatternUnlock(N, hits):
         if i != "0":
             result += i
     return result
+
+
+def checkOnDiagonal(Points, current, next):
+    if 1 < (abs(Points.get(current)[0] - Points.get(next)[0]) +
+            abs(Points.get(current)[1] - Points.get(next)[1])):
+        return True
+    return False
+
+
+def toFixed(number, digits=0):
+    return f"{number:.{digits}f}"
