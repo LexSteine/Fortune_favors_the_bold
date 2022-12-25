e63df09ed8535179f362c9a7abf42e5de56cfc8a
LexSteine <112965317+LexSteine@users.noreply.github.com>
2022-12-25 20:54:02 +0300

    Update Task12.py

--- Task12.py
+++ Task12.py
@@ -1,7 +1,7 @@
 def MassVote(N, Votes):
-    if CheckCountOfWinners(Votes, FindMaxNumber(Votes)) and FindPercent(Votes, FindMaxNumber(Votes)) > "50.000":
+    if CheckCountOfWinners(Votes, FindMaxNumber(Votes)) and float(FindPercent(Votes, FindMaxNumber(Votes))) > 50.000:
         return f"majority winner {Votes.index(FindMaxNumber(Votes)) + 1}"
-    elif CheckCountOfWinners(Votes, FindMaxNumber(Votes)) and FindPercent(Votes, FindMaxNumber(Votes)) <= "50.000":
+    elif CheckCountOfWinners(Votes, FindMaxNumber(Votes)) and float(FindPercent(Votes, FindMaxNumber(Votes))) <= 50.000:
         return f"minority winner {Votes.index(FindMaxNumber(Votes)) + 1}"
     else:
         return "no winner"
