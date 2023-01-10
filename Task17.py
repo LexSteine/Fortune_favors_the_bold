c8ddc1376ade2dd2330a2739f7deedecd3719fb4
LexSteine <112965317+LexSteine@users.noreply.github.com>
2023-01-10 18:45:39 +0300

    Я долбоёб, опять сука принт пропустил!!!!!

--- Task17.py
+++ Task17.py
@@ -3,7 +3,6 @@ def LineAnalysis(line):
     line = line.split("*")
     line.pop(0)
     line.pop(len(line) - 1)
-    print(line, result)
     for i in line:
         if i != line[0]:
             result = False
