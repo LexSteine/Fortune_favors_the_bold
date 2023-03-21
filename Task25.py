c622df6259b1df0ff85da91d3f9e1683d42cf24a
LexSteine <112965317+LexSteine@users.noreply.github.com>
2023-03-21 14:10:22 +0300

    Update Task25.py

--- Task25.py
+++ Task25.py
@@ -1,15 +1,15 @@
 def TransformTransform(A, N):
     Result = S(S(A))
     summa = 0
-    for i in range(len(Result) - 1):
+    for i in range(len(Result)):
         summa += Result[i]
     return summa % 2 == 0 and summa !=0
 
 
 def S(A):
     B = []
-    for i in range(len(A) - 1): #N
-        for j in range(len(A) - 1 - i): #0.5N
+    for i in range(len(A)): #N
+        for j in range(len(A) - i): #0.5N
             k = i + j
             B.append(max(A[j:k+1])) #N
     return B
